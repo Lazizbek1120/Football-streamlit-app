97ac0edfdbe57e36e7521bad610cb6a51b040e74
Lazizbek1120 <laziztoshpolatov482@gmail.com>
2026-02-23 09:56:09 +0500

    Update chempion.py

--- chempion.py
+++ chempion.py
@@ -19,28 +19,28 @@ df["away_goals"] = pd.to_numeric(df["away_goals"], errors="coerce")
 df["total_goals"] = df["home_goals"] + df["away_goals"]
 
 
-# 1️ ENG KO‘P GOL URGAN JAMOA
+# 1️ ENG KOP GOL URGAN JAMOA
 
 home_goals = df.groupby("home_team")["home_goals"].sum()
 away_goals = df.groupby("away_team")["away_goals"].sum()
 
 total_scored = home_goals.add(away_goals, fill_value=0).sort_values(ascending=False)
 
-st.subheader(" Eng ko‘p gol urgan jamoalar")
+st.subheader(" Eng kop gol urgan jamoalar")
 fig1 = px.bar(total_scored.head(10),
               title="Top 10 Goal Scoring Teams",
               labels={"value":"Goals","index":"Team"})
 st.plotly_chart(fig1, use_container_width=True)
 
 
-# 2️ ENG KO‘P GOL O‘TKAZGAN JAMOA
+# 2️ ENG KOP GOL O‘TKAZGAN JAMOA
 
 home_conceded = df.groupby("home_team")["away_goals"].sum()
 away_conceded = df.groupby("away_team")["home_goals"].sum()
 
 total_conceded = home_conceded.add(away_conceded, fill_value=0).sort_values(ascending=False)
 
-st.subheader(" Eng ko‘p gol o‘tkazgan jamoalar")
+st.subheader(" Eng kop gol otkazgan jamoalar")
 fig2 = px.bar(total_conceded.head(10),
               title="Most Goals Conceded",
               labels={"value":"Goals","index":"Team"})
@@ -49,7 +49,7 @@ st.plotly_chart(fig2, use_container_width=True)
 
 # 3️ UY VS SAFAR STATISTIKASI
 
-st.subheader(" Uy vs ✈ Safar Statistikasi")
+st.subheader(" Uy vs Safar Statistikasi")
 
 home_total = df["home_goals"].sum()
 away_total = df["away_goals"].sum()
@@ -59,9 +59,9 @@ col1.metric("Uyda urilgan jami gollar", int(home_total))
 col2.metric("Safarda urilgan jami gollar", int(away_total))
 
 
-# 4️ ENG NATIJADOR O‘YINLAR
+# 4️ ENG NATIJADOR OYINLAR
 
-st.subheader(" Eng natijador o‘yinlar")
+st.subheader(" Eng natijador oyinlar")
 
 top_matches = df.sort_values("total_goals", ascending=False).head(10)
 
@@ -70,12 +70,12 @@ st.dataframe(
 )
 
 
-# 5️ O‘RTACHA GOL KO‘RSATKICHI
+# 5️ ORTACHA GOL KORSATKICHI
 
-st.subheader(" O‘rtacha gol ko‘rsatkichi")
+st.subheader(" Ortacha gol ko‘rsatkichi")
 
 avg_goals = df["total_goals"].mean()
-st.metric("Har o‘yinga o‘rtacha gol", round(avg_goals, 2))
+st.metric("Har oyinga ortacha gol", round(avg_goals, 2))
 
 fig3 = px.histogram(df, x="total_goals", nbins=12,
                     title="Goal Distribution Per Match")
@@ -118,3 +118,4 @@ predicted_champion = table.index[0]
 
 
 st.success(f" Bashorat qilingan chempion: {predicted_champion}")
+
